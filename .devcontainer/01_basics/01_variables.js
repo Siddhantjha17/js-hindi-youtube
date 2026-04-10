6bb7dec8f1fe37f4d2f2902307e589e72adbcfeb
Siddhant Suman <146611822+Siddhantjha17@users.noreply.github.com>
2026-04-10 10:20:17 +0000

    further

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "harsh@google.com"
+var accountPassword = "12345"
+accountCity = "Samastipur"
+
+// accountId = 2
+accountEmail = "abc@gmail.com"
+accountPassword = "44321334"
+accountCity = "Bhopal"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity])
